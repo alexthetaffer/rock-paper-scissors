35621ffd644c85b20a96206c7483724f361a2150
alex <alexthetaffer@gmail.com>
2022-08-04 20:35:34 +0300

    Add playRound function

--- javascript.js
+++ javascript.js
@@ -1,15 +1,52 @@
 
 function getComputerChoice() {
 
-    // Generate random number 0-2
+    // Generate random number [0..2]
     choice = Math.floor(Math.random()*3);
 
     switch (choice) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors";
+            return "scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    // Convert user input to lowercase
+    playerSelection = playerSelection.toLowerCase();
+    
+    // Player chooses rock
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            return ("You loose! Paper beats rock");
+        } else if (computerSelection === 'scissors') {
+            return ("You win! Rock beats scissors");
+        } else {
+            return ("A tie!");
+        }
+    // Player chooses paper
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            return ("You win! Paper beats rock");
+        } else if (computerSelection === 'scissors') {
+            return ("You loose! Scissors beat paper");
+        } else {
+            return ("A tie!");
+        }
+    // Player chooses scissors
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            return ("You loose! Rock beats scissors");
+        } else if (computerSelection === 'paper') {
+            return ("You win! Scissors beat paper")
+        } else {
+            return ("A tie!");
+        }
+    } else {
+        return ("Something's gone wrong");
     }
 }
