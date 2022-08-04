2d3420b9836a3918bec36b649fedff48437a7c59
alex <alexthetaffer@gmail.com>
2022-08-04 19:39:28 +0300

    Clean up code

--- javascript.js
+++ javascript.js
@@ -3,7 +3,6 @@ function getComputerChoice() {
 
     // Generate random number 0-2
     choice = Math.floor(Math.random()*3);
-    console.log(`choice: ${choice}`);
 
     switch (choice) {
         case 0:
@@ -12,10 +11,5 @@ function getComputerChoice() {
             return "Paper";
         case 2:
             return "Scissors";
-    }
-
-    for (i = 0; i < 100; i++) {
-        console.log(getComputerChoice());
-
     }
 }
